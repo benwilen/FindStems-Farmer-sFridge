21fd63ff7bb567e26d1dc52d767c6ac5fd85ffe0
benwilen <bcw3@williams.edu>
2022-04-18 23:25:19 -0400

    Update

---
+++ app/src/main/java/com/example/findstems/Tab1Fragment.java
@@ -0,0 +1,63 @@
+package com.example.findstems;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.*;
+
+public class Tab1Fragment extends Fragment {
+    private static final String TAG = "Tab1Fragment";
+
+    private Button btnEnterWord;
+    private EditText editTextInput;
+    private TextView printStems;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab1_fragment,container,false);
+        btnEnterWord = (Button) view.findViewById(R.id.btnEnterWord);
+        editTextInput = (EditText) view.findViewById(R.id.editTextInput);
+        printStems = (TextView) view.findViewById(R.id.printStems);
+
+        btnEnterWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //get text from input
+                String input = editTextInput.getText().toString();
+
+                //convert string to list of all words entered
+               String[] allWords = input.split(" ");
+
+               StringBuilder sb = new StringBuilder();
+               for (int i = 0 ; i < allWords.length ; i++) {
+                   String[] stems = Word.getStems(allWords[i]).toArray(new String[0]);
+                   sb.append(allWords[i] + ": ");
+
+                   for (int j = 0 ; j < stems.length - 1 ; j++) {
+                       sb.append(stems[j] + ", ");
+                   }
+                   sb.append(stems[stems.length - 1]);
+
+                   sb.append("\n");
+                }
+
+               printStems.setText(sb.toString());
+            }
+        });
+
+        return view;
+    }
+
+}
